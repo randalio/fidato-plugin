5fe9702e080d71ec96448c22865304d704e99170
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-03-25 16:07:01 -0400

    updated version number

--- fidato.php
+++ fidato.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Fidato Wealth
  * Description: Plugin with Webpack build system
- * Version: 1.0.14
+ * Version: 1.0.16
  * Author: Randal Pope
  */
 
@@ -276,7 +276,7 @@ add_action('admin_init', 'fidato_increase_memory_limit');
 
 
 class MyPlugin {
-    private $version = '1.0.14';
+    private $version = '1.0.16';
 
     public function __construct() {
         add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_assets'));
